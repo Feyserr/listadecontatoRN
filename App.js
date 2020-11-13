b629146fcca0278af278916e77dfd150d9ad2bf4
Feyserr <doo_dener@hotmail.com>
2020-11-12 21:32:36 -0300

    segundo commit

--- App.js
+++ App.js
@@ -1,32 +1,32 @@
-import React from 'react';
+import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
-import {NavigationContainer, StackActions} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack'
-import {createDrawerNavigator} from '@react-navigation/drawer'
-import PeopleDetailsPage from './src/pages/PeopleDetailsPage';
-import PeoplePage  from './src/pages/PeoplePage'
-const Stack = createStackNavigator();
-//const Drawer = createDrawerNavigator();
-function App(){
-  return(
-    <View style={style.container}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="PeoplePage">
-          <Stack.Screen name="Lista de Pessoas" component={PeoplePage} />
-          <Stack.Screen name="Detalhe da Pessoa" component={PeopleDetailsPage} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
-  )
+import { NavigationContainer } from '@react-navigation/native';
+
+import PeoplePage from './src/pages/PeoplePage'
+import PeopleDetailsPage from './src/pages/PeopleDetailsPage'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+
+const Drawer = createDrawerNavigator();
+function App() {
+  return (  
+    <View style={styles.container}>
+        <NavigationContainer >
+          <Drawer.Navigator initialRouteName="PeoplePage">
+            <Drawer.Screen name="Lista de Pessoas" component={PeoplePage} />
+            <Drawer.Screen name="Detalhe da Pessoa" component={PeopleDetailsPage} />
+          </Drawer.Navigator>
+        </NavigationContainer>
+      </View>
+  );
 }
 
-const style = StyleSheet.create(
-  {
-    container: {
-      flex: 1,
-      backgroundColor: '#fff' 
-    }
-  }
-)
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
 
 export default App;
